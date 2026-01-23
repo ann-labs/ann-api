44bbeac9484d8b5ca50de2d93c68c5d576afc088
ANN <andikagung49@gmail.com>
2026-01-23 19:33:14 +0700

    Update hentai.js

--- api/hentai.js
+++ api/hentai.js
@@ -5,7 +5,7 @@ const URL = "https://nekopoi.care/category/hentai/";
 const UA =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
 
-export async function getHentai(req, res) {
+export default async function getHentai(req, res) {
   try {
     const { data: html } = await axios.get(URL, {
       headers: {
