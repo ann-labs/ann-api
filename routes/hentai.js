f4cf9c2bc648bbbd7425ecdece216b1317c80afe
ANN <andikagung49@gmail.com>
2026-01-26 09:43:49 +0700

    Update hentai.js

--- routes/hentai.js
+++ routes/hentai.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getHentai } from "../controllers/hentai.js";
+import { hentai } from "../controllers/hentai.js"
 
 const router = express.Router();
 
-router.get("/", getHentai);
+router.get("/", async (req, res) => {
+  const result = await hentai();
+
+  if (!result.status) {
+    return res.status(500).json(result);
+  }
+
+  res.json(result);
+});
 
 export default router;
